35d289bf93e40a45bf6ef446427eaa17f49085a2
yogesh4492 <you@example.com>
2025-09-05 11:23:31 +0530

    new file:   list/methods/1.py 	new file:   list/methods/append.ipynb 	new file:   list/methods/extend.ipynb 	new file:   list/methods/insert.ipynb 	renamed:    list/practice/Ex2.ipynb -> list/practice/Accesssing_list.ipynb 	renamed:    list/practice/ex1.ipynb -> list/practice/creating_list.ipynb 	new file:   list/practice/updating_list.ipynb

---
+++ list/methods/1.py
@@ -0,0 +1,28 @@
+# list=[1,2,5,6,7]
+# size=int(input("Enter Element size= "))
+# for i in range(size):
+#     index=int(input("Enter Index= "))
+#     element=int(input("Enter Element= "))
+#     list.insert(index,element)
+# print(list)
+
+#without any built in
+# list=[1,5,95,78]
+# pos,ele=2,5
+# length=len(list)
+# for i in list:
+#     print(list[i])
+# #     if list[i]==pos:
+# #         i=element
+# # print(list)
+
+# list=[1,35,95,78]
+# pos,ele=2,5
+# length=len(list)
+# for i in range(length):
+
+#     if i==pos:
+#         list = list+ [ele]
+# print(list)
+
+
